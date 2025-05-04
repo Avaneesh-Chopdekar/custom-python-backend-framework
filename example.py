80a0e717a9fb644093aa053c72ef2dfd070f049f
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-05-04 18:33:09 +0530

    setup global and local middlewares

--- example.py
+++ example.py
@@ -1,10 +1,19 @@
+from datetime import datetime
 from main import PyApi
 
 
-pyapi = PyApi()
+def logger_middleware(request):
+    print(f'[{datetime.now()}] {request['REQUEST_METHOD']} {request["PATH_INFO"]}')
 
 
-@pyapi.get("/hello/{id}")
+def local_middleware(request):
+    print("Local Middleware")
+
+
+pyapi = PyApi(middlewares=[logger_middleware])
+
+
+@pyapi.get("/hello/{id}", middlewares=[local_middleware])
 def hello(request, response, id):
     response.send(f"Hello {id}")
 
--- main.py
+++ main.py
@@ -3,20 +3,35 @@ from parse import parse
 
 
 class PyApi:
-    def __init__(self):
+    def __init__(self, middlewares=[]):
         self.routes = dict()
+        self.middlewares = middlewares
+        self.route_middlewares = dict()
 
     def __call__(self, environ, start_response):
         response = Response(status_code="404 Not Found", text="Not Found")
+
+        for middleware in self.middlewares:
+            if callable(middleware):
+                middleware(environ)
+            else:
+                raise TypeError("Middleware must be callable")
+
         for path, handler_dict in self.routes.items():
             res = parse(path, environ["PATH_INFO"])
             for request_method, handler in handler_dict.items():
                 if res and environ["REQUEST_METHOD"] == request_method:
+                    middleware_list = self.route_middlewares[path][request_method]
+                    for middleware in middleware_list:
+                        if callable(middleware):
+                            middleware(environ)
+                        else:
+                            raise TypeError("Middleware must be callable")
                     handler(environ, response, **res.named)
 
         return response.as_wsgi(start_response)
 
-    def __map_route_to_handler(self, path, request_method, handler):
+    def __map_route_to_handler(self, path, request_method, handler, middlewares):
         # {
         #     '/hello': {
         #         'GET': handler
@@ -27,35 +42,47 @@ class PyApi:
             self.routes[path_name] = dict()
 
         self.routes[path_name][request_method] = handler
+
+        # {
+        #     '/hello': {
+        #         'GET': [middleware1, middleware2]
+        #     }
+        # }
+
+        if path_name not in self.route_middlewares:
+            self.route_middlewares[path_name] = dict()
+
+        self.route_middlewares[path_name][request_method] = middlewares
+
         return handler
 
-    def get(self, path=None):
+    def get(self, path=None, middlewares=[]):
         def wrapper(handler):
-            return self.__map_route_to_handler(path, "GET", handler)
+            return self.__map_route_to_handler(path, "GET", handler, middlewares)
 
         return wrapper
 
-    def post(self, path=None):
+    def post(self, path=None, middlewares=[]):
         def wrapper(handler):
-            return self.__map_route_to_handler(path, "POST", handler)
+            return self.__map_route_to_handler(path, "POST", handler, middlewares)
 
         return wrapper
 
-    def put(self, path=None):
+    def put(self, path=None, middlewares=[]):
         def wrapper(handler):
-            return self.__map_route_to_handler(path, "PUT", handler)
+            return self.__map_route_to_handler(path, "PUT", handler, middlewares)
 
         return wrapper
 
-    def patch(self, path=None):
+    def patch(self, path=None, middlewares=[]):
         def wrapper(handler):
-            return self.__map_route_to_handler(path, "PATCH", handler)
+            return self.__map_route_to_handler(path, "PATCH", handler, middlewares)
 
         return wrapper
 
-    def delete(self, path=None):
+    def delete(self, path=None, middlewares=[]):
         def wrapper(handler):
-            return self.__map_route_to_handler(path, "DELETE", handler)
+            return self.__map_route_to_handler(path, "DELETE", handler, middlewares)
 
         return wrapper
 
