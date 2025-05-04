5a70c1b296265de90f905539e9384d3f2defeb9a
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-05-04 17:27:30 +0530

    route based handler will be called

--- example.py
+++ example.py
@@ -2,3 +2,10 @@ from main import PyApi
 
 
 pyapi = PyApi()
+
+
+@pyapi.get("/hello")
+def hello(request, response):
+    response["status_code"] = "200 OK"
+    response["headers"] = [("Content-Type", "text/plain")]
+    response["text"] = "Hello, World!"
--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 class PyApi:
     def __init__(self):
-        pass
+        self.routes = dict()
 
     def __call__(self, environ, start_response):
-        print(environ)
-        start_response("200 OK", [("Content-Type", "text/plain")])
-        return [b"Hello Avaneesh!"]
+        response = {}
+        for path, handler_dict in self.routes.items():
+            for request_method, handler in handler_dict.items():
+                if (
+                    environ["PATH_INFO"] == path
+                    and environ["REQUEST_METHOD"] == request_method
+                ):
+                    handler(environ, response)
+                    start_response(response["status_code"], response["headers"])
+                    return [(response["text"]).encode()]
+
+        status_code = "404 Not Found"
+        headers = [("Content-Type", "text/plain")]
+        body = b"Not Found"
+
+        start_response(status_code, headers)
+        return [body]
+
+    def get(self, path=None):
+        def wrapper(handler):
+            # {
+            #     '/hello': {
+            #         'GET': handler
+            #     }
+            # }
+            path_name = path if path else f"/{handler.__name__}"
+            if path_name not in self.routes:
+                self.routes[path_name] = dict()
+
+            self.routes[path_name]["GET"] = handler
+
+            # print(self.routes)
+
+        return wrapper
 
 
 pyapi = PyApi()
